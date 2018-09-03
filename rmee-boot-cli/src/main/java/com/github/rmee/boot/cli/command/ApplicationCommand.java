7ea34f3b3a1ea15fa4c27a82a108a9284545513e
Remo <remo.meier@adnovum.ch>
2018-09-03 17:44:13 +0200

    properly display spring configuration validation errors

--- rmee-boot-cli/src/main/java/com/github/rmee/boot/cli/command/ApplicationCommand.java
+++ rmee-boot-cli/src/main/java/com/github/rmee/boot/cli/command/ApplicationCommand.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import com.github.rmee.boot.cli.util.ApplicationCliOutput;
@@ -11,11 +13,37 @@ import picocli.CommandLine;
 
 public abstract class ApplicationCommand {
 
-	@CommandLine.Option(names = {"-h", "--help"}, help = true, description = "Prints help message and exits")
+	@CommandLine.Option(names = { "-h", "--help" }, help = true, description = "Prints help message and exits")
 	protected boolean help = false;
 
-	@CommandLine.Option(names = {"-d", "--debug"}, split = ",", description = "Shows debug information during execution.")
+	@CommandLine.Option(names = { "-d", "--debug" }, split = ",", description = "Shows debug information during execution.")
 	protected String[] debug = null;
+
+	@CommandLine.Option(names = { "-D", "--property" }, paramLabel = "KEY=VALUE")
+	private Map<String, String> properties = new LinkedHashMap<>();
+
+	@CommandLine.Spec
+	private CommandLine.Model.CommandSpec spec;
+
+	public Map<String, String> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Map<String, String> map) {
+		for (String key : map.keySet()) {
+			String newValue = map.get(key);
+			validateUnique(key, newValue);
+			properties.put(key, newValue);
+		}
+	}
+
+	private void validateUnique(String key, String newValue) {
+		String existing = properties.get(key);
+		if (existing != null && !existing.equals(newValue)) {
+			throw new CommandLine.ParameterException(spec.commandLine(),
+					String.format("Duplicate key '%s' for values '%s' and '%s'.", key, existing, newValue));
+		}
+	}
 
 	public void run(ApplicationCommandContext context) {
 		if (debug != null) {
@@ -25,9 +53,12 @@ public abstract class ApplicationCommand {
 			showHelp(context);
 		}
 		else {
+			for (Map.Entry<String, String> property : properties.entrySet()) {
+				System.setProperty(property.getKey(), property.getValue());
+			}
+
 			execute(context);
 		}
-
 	}
 
 	protected void showHelp(ApplicationCommandContext context) {
--- rmee-boot-cli/src/main/java/com/github/rmee/boot/cli/provider/SpringBeanProvider.java
+++ rmee-boot-cli/src/main/java/com/github/rmee/boot/cli/provider/SpringBeanProvider.java
@@ -1,7 +1,16 @@
 package com.github.rmee.boot.cli.provider;
 
+
+import com.github.rmee.boot.cli.CliException;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBindException;
+import org.springframework.boot.context.properties.bind.BindException;
+import org.springframework.boot.context.properties.bind.validation.BindValidationException;
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 
 public class SpringBeanProvider implements BeanProvider {
 
@@ -28,8 +37,47 @@ public class SpringBeanProvider implements BeanProvider {
 			throw new IllegalStateException("already configured");
 		}
 
-		application = new SpringApplication(configurationClass);
-		application.setWebEnvironment(false);
-		context = application.run();
+		try {
+			application = new SpringApplication(configurationClass);
+			application.setWebEnvironment(false);
+			context = application.run();
+		}
+		catch (UnsatisfiedDependencyException e) {
+			// give message message in cause of configuration errrors
+			if (e.getCause() instanceof ConfigurationPropertiesBindException) {
+				Throwable bindException = e.getCause();
+				if (bindException.getCause() instanceof BindException) {
+					if (bindException.getCause().getCause() instanceof BindValidationException) {
+						BindValidationException validationException =
+								(BindValidationException) bindException.getCause().getCause();
+						ValidationErrors validationErrors = validationException.getValidationErrors();
+
+						StringBuilder builder = new StringBuilder();
+						builder.append("validation of configuration properties failed:");
+						for (ObjectError error : validationErrors.getAllErrors()) {
+							builder.append("\n- ");
+							builder.append(error.getObjectName());
+							if (error instanceof FieldError) {
+								builder.append(".");
+								builder.append(((FieldError) error).getField());
+								builder.append("=");
+								Object rejectedValue = ((FieldError) error).getRejectedValue();
+								if (rejectedValue != null) {
+									builder.append("null");
+								}
+								else {
+									builder.append(rejectedValue);
+								}
+							}
+							builder.append(" ");
+							builder.append(error.getDefaultMessage());
+						}
+
+						throw new CliException(builder.toString());
+					}
+				}
+			}
+			throw e;
+		}
 	}
 }
