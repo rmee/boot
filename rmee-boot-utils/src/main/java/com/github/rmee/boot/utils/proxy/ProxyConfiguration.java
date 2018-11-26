dda81f7ff2b8a6bdf7071e8b3f0717667f6166ff
Remo <remo.meier@adnovum.ch>
2018-11-26 14:12:01 +0100

    honor environment proxy configuration

--- rmee-boot-utils/src/main/java/com/github/rmee/boot/utils/proxy/ProxyConfiguration.java
+++ rmee-boot-utils/src/main/java/com/github/rmee/boot/utils/proxy/ProxyConfiguration.java
@@ -1,5 +1,7 @@
 package com.github.rmee.boot.utils.proxy;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -10,7 +12,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @ConfigurationProperties("proxy")
-@EnableConfigurationProperties({ProxyProperties.class})
+@EnableConfigurationProperties({ ProxyProperties.class })
 public class ProxyConfiguration implements InitializingBean {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProxyConfiguration.class);
@@ -34,8 +36,29 @@ public class ProxyConfiguration implements InitializingBean {
 				props.put("no_proxy", properties.getExclude());
 			}
 		}
+		else if (System.getenv("HTTP_PROXY") != null || System.getenv("HTTPS_PROXY") != null) {
+			setupProxy("http");
+			setupProxy("https");
+		}
 		else {
 			LOGGER.debug("no proxy configured");
+		}
+	}
+
+	private static void setupProxy(String name) {
+		String envName = name.toUpperCase() + "_PROXY";
+		String value = System.getenv(envName);
+		if (value != null) {
+			URL url;
+			try {
+				url = new URL(value);
+			}
+			catch (MalformedURLException e) {
+				throw new IllegalStateException("invalid proxy url " + value + " in env " + envName);
+			}
+
+			System.setProperty(name + ".proxyHost", url.getHost());
+			System.setProperty(name + ".proxyPort", Integer.toString(url.getPort()));
 		}
 	}
 }
