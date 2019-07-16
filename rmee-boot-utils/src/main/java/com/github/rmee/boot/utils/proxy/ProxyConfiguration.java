0a1324eaa3fa1f3c5c862895b6c60f068e099466
Remo <remo.meier@adnovum.ch>
2019-07-16 12:00:46 +0200

    allow proxy discovery outside of Spring Boot

--- rmee-boot-utils/src/main/java/com/github/rmee/boot/utils/proxy/ProxyConfiguration.java
+++ rmee-boot-utils/src/main/java/com/github/rmee/boot/utils/proxy/ProxyConfiguration.java
@@ -1,10 +1,6 @@
 package com.github.rmee.boot.utils.proxy;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +19,7 @@ public class ProxyConfiguration implements InitializingBean {
 	private ProxyProperties properties;
 
 	@Override
-	public void afterPropertiesSet() throws Exception {
+	public void afterPropertiesSet() {
 		if (properties.getHost() != null && properties.getPort() > 0) {
 			LOGGER.debug("using proxy {}:{}", properties.getHost(), properties.getPort());
 
@@ -37,46 +33,9 @@ public class ProxyConfiguration implements InitializingBean {
 				props.put("no_proxy", properties.getExclude());
 			}
 		}
-		else if (getEnv("HTTP_PROXY") != null || getEnv("HTTPS_PROXY") != null) {
-			setupProxy("http");
-			setupProxy("https");
-		}
 		else {
-			LOGGER.debug("no proxy configured");
+			ProxyDiscovery.readFromEnv();
 		}
-
-		String no_proxy = getEnv("NO_PROXY");
-		if (no_proxy != null) {
-			// see https://docs.oracle.com/javase/7/docs/api/java/net/doc-files/net-properties.html
-			String javaNoProxy = Arrays.asList(no_proxy.split("\\,")).stream().map(it -> it.startsWith(".") ? "*" + it : it).collect(Collectors.joining("|"));
-			System.setProperty("http.nonProxyHosts", javaNoProxy);
-			System.setProperty("https.nonProxyHosts", javaNoProxy);
-		}
-	}
-
-	private static void setupProxy(String name) {
-		String envName = name.toUpperCase() + "_PROXY";
-		String value = getEnv(envName);
-		if (value != null) {
-			URL url;
-			try {
-				url = new URL(value);
-			}
-			catch (MalformedURLException e) {
-				throw new IllegalStateException("invalid proxy url " + value + " in env " + envName);
-			}
-
-			System.setProperty(name + ".proxyHost", url.getHost());
-			System.setProperty(name + ".proxyPort", Integer.toString(url.getPort()));
-		}
-	}
-
-	private static String getEnv(String envName) {
-		String value = System.getenv(envName.toUpperCase());
-		if (value == null) {
-			value = System.getenv(envName.toLowerCase());
-		}
-		return value;
 	}
 }
 
